a0146c28c61cbe25dd1e3c61a4ef3c37bca8275b
safin878 <shafin6747@gmail.com>
2024-04-26 10:07:58 +0600

    Sweet alert add

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,84 +1,148 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/Authprovider";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const { userCreate } = useContext(AuthContext);
 
   const handelonReg = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const name = form.name.value;
-    const email = form.email.value;
-    const photo = form.photo.value;
-    const password = form.password.value;
+    // e.preventDefault();
+    const { name, email, photo, password } = e;
+
     console.log(name, email, photo, password);
     userCreate(email, password)
       .then((result) => {
         // Signed in
         console.log(result.user);
-
+        Swal.fire({
+          title: "success",
+          text: "Registration successful",
+          icon: "success",
+          confirmButtonText: "Ok",
+        });
         // ...
       })
       .catch((error) => {
         console.log(error);
+        Swal.fire({
+          title: "Error!",
+          text: "Something went wrong",
+          icon: "error",
+          confirmButtonText: "OK",
+        });
       });
   };
   return (
     <div>
-      <div className="hero   bg-base-200">
+      <div className="hero min-h-screen  container mx-auto">
         <div className="hero-content w-full flex-col ">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Register now!</h1>
           </div>
           <div className="card shrink-0  shadow-2xl bg-base-100">
-            <form onSubmit={handelonReg} className="card-body">
-              <div className="form-control">
+            <form onSubmit={handleSubmit(handelonReg)} className="w-96 p-6">
+              <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
                 <input
+                  {...register("name", {
+                    required: {
+                      value: true,
+                      message: "Name is required",
+                    },
+                  })}
                   type="text"
                   placeholder="Write Your Name"
                   className="input input-bordered"
                   name="name"
-                  required
                 />
+                <div>
+                  {errors.name && (
+                    <p className="text-red-600">{errors.name.message}</p>
+                  )}
+                </div>
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "Email is required",
+                    },
+                  })}
                   type="email"
                   placeholder="email"
                   className="input input-bordered"
                   name="email"
-                  required
                 />
+
+                <div>
+                  {errors.email && (
+                    <p className="text-red-600">{errors.email.message}</p>
+                  )}
+                </div>
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">PhotoUrl</span>
                 </label>
                 <input
+                  {...register("photo", {
+                    required: {
+                      value: true,
+                      message: "Photo Url is required",
+                    },
+                  })}
                   type="text"
                   placeholder="Photo URL"
                   className="input input-bordered"
                   name="photo"
-                  required
                 />
+                <div>
+                  {errors.photo && (
+                    <p className="text-red-600">{errors.photo.message}</p>
+                  )}
+                </div>
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
                 <input
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "Password is required",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "Password must be at least 6 characters",
+                    },
+                    pattern: {
+                      value: /^(?=.*[a-z])(?=.*[A-Z])/,
+                      message:
+                        "Password must contain one uppercase letter and one lowercase letter",
+                    },
+                  })}
                   type="password"
                   placeholder="password"
                   className="input input-bordered"
                   name="password"
-                  required
                 />
+                {errors.password && (
+                  <p className="text-red-600">{errors.password.message}</p>
+                )}
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">
                     Forgot password?
